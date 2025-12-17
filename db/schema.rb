a69eebc642a5e3b808fbd574dc68a6a7bb672f9a
Baba0608 <babafakruddinnitturu@gmail.com>
2025-12-17 20:05:22 +0530

    added password to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_17_030008) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_17_143353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,5 +19,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_17_030008) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 end
