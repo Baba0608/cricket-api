3e30d5d5723216a01e88879503cb8ff6a3473d1c
Baba0608 <babafakruddinnitturu@gmail.com>
2025-12-26 16:30:15 +0530

    reject match invite

--- app/controllers/api/v1/match_invites_controller.rb
+++ app/controllers/api/v1/match_invites_controller.rb
@@ -23,6 +23,22 @@ class Api::V1::MatchInvitesController < ApplicationController
   end
 
   def reject
+    if @match_invite.accepted? || @match_invite.rejected?
+      return render json: { errors: [ "Invite already accepted or rejected" ] }, status: :unprocessable_content
+    end
+
+    current_team = current_user.player.teams.first
+    receiving_team = @match_invite.receive_by_team_id
+
+    if receiving_team != current_team.id
+      return render json: { errors: [ "You are not allowed to perform this acction", "Only received team can reject the invite" ] }, status: :forbidden
+    end
+
+    if @match_invite.update(status: :rejected)
+      render json: { match_invite: @match_invite }
+    else
+      render json: { errors: @match_invite.errors }, status: :unprocessable_content
+    end
   end
 
   private
