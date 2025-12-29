d831ccde6e839e105c85e4ebdb5b2f4e393f6d7b
Baba0608 <babafakruddinnitturu@gmail.com>
2025-12-29 12:26:25 +0530

    Invite players to a match

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::MatchesController < ApplicationController
-  before_action :set_match, only: %i[ show update destroy ]
+  before_action :set_match, only: %i[ show update destroy invite_player ]
 
   # GET /matches
   def index
@@ -11,17 +11,6 @@ class Api::V1::MatchesController < ApplicationController
   # GET /matches/1
   def show
     render json: @match
-  end
-
-  # POST /matches
-  def create
-    @match = Match.new(match_params)
-
-    if @match.save
-      render json: @match, status: :created, location: @match
-    else
-      render json: @match.errors, status: :unprocessable_content
-    end
   end
 
   # PATCH/PUT /matches/1
@@ -37,6 +26,47 @@ class Api::V1::MatchesController < ApplicationController
   def destroy
     @match.destroy!
   end
+
+  # POST /matches/1/invite_player
+  def invite_player
+    current_team = current_user.player&.teams&.first
+      return render json: { errors: [ "Please create Profile/Team first to invite players" ] },
+                status: :forbidden unless current_team
+
+    team_id = params.dig("match", "team_id")
+    player_id = params.dig("match", "player_id")
+
+    if current_team.id != team_id
+      return render json: { errors: [ "You can not invite players for other team" ] }, status: :forbidden
+    end
+
+    # check if already invited
+    if @match.match_player_invites.exists?(team_id: team_id, player_id: player_id)
+      return render json: { errors: [ "Player is already invited" ] },
+                  status: :unprocessable_entity
+    end
+
+    player = Player.find_by(id: player_id)
+    unless player
+      return render json: { errors: [ "Player is not available" ] },
+                    status: :unprocessable_entity
+    end
+
+    invite = @match.match_player_invites.build(
+      team_id: team_id,
+      player_id: player_id,
+      status: :pending
+    )
+
+    if invite.save
+      render json: { match_player_invite: invite }, status: :created
+    else
+      render json: { errors: invite.errors.full_messages },
+            status: :unprocessable_entity
+    end
+  end
+
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
